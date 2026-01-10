32089bdf73dc10579bac08813c5c669a6d5ebcc0
matej <matejmedveczky@gmail.com>
2026-01-10 17:40:18 +0100

    reversed incorrect delete

--- Core/Src/control.c
+++ Core/Src/control.c
@@ -15,6 +15,37 @@ typedef struct {
 static PID_State pid_linear = {0};
 static PID_State pid_angular = {0};
 
+void DC_Motor_Init(DC_Motor *motor, TIM_HandleTypeDef *htim, uint32_t channel,
+                   GPIO_TypeDef *dir1_port, uint16_t dir1_pin,
+                   GPIO_TypeDef *dir2_port, uint16_t dir2_pin) {
+    motor->htim = htim;
+    motor->channel = channel;
+    motor->dir1_port = dir1_port;
+    motor->dir1_pin = dir1_pin;
+    motor->dir2_port = dir2_port;
+    motor->dir2_pin = dir2_pin;
+}
+
+void DC_Motor_Set(DC_Motor *motor, float speed) {
+    uint16_t max_pwm = __HAL_TIM_GET_AUTORELOAD(motor->htim);
+
+    float clamped = speed;
+    if (clamped > MAX_SPEED) clamped = MAX_SPEED;
+    if (clamped < -MAX_SPEED) clamped = -MAX_SPEED;
+
+    if (clamped >= 0) {
+        HAL_GPIO_WritePin(motor->dir1_port, motor->dir1_pin, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(motor->dir2_port, motor->dir2_pin, GPIO_PIN_SET);
+    } else {
+        HAL_GPIO_WritePin(motor->dir1_port, motor->dir1_pin, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(motor->dir2_port, motor->dir2_pin, GPIO_PIN_RESET);
+        clamped = -clamped;
+    }
+
+    uint16_t pwm = (uint16_t)((clamped / MAX_SPEED) * max_pwm);
+    __HAL_TIM_SET_COMPARE(motor->htim, motor->channel, pwm);
+}
+
 void tank_control(DC_Motor *left, DC_Motor *right,
                   float v_linear_desired, float v_angular_desired,
                   float v_linear_actual, float v_angular_actual,
